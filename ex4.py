4ea853350fdc59610022830753dd52775da8c468
wassupx <sonechka05062007@gmail.com>
2026-03-14 11:13:56 +0700

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -4,3 +4,4 @@ with open ("input.txt", "r", encoding="utf-8") as f:
         if len(line) > 20:
             with open ("output.txt", "a", encoding="utf-8") as fl:
                 fl.write(line)
+                

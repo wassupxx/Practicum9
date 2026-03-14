37e40bd7b93495cf9125b743907cef9d1a76ee7f
wassupx <sonechka05062007@gmail.com>
2026-03-14 11:13:14 +0700

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -6,3 +6,4 @@ with open("input.txt", "r", encoding="utf-8") as f:
         if line[0][0] == "a" or line[0][0] == "A":
             with open("output.txt", "a", encoding="utf-8") as fi:
                 fi.write(line)
+                

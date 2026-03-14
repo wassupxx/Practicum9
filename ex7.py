29047d90e720baa775b701dfbdd65b0784b653ea
wassupx <sonechka05062007@gmail.com>
2026-03-14 11:16:01 +0700

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -3,3 +3,4 @@ with open ("input.txt","r", encoding="utf-8") as f:
 cleaned = [line for line in lines if line.strip() != "100"]
 with open ("input.txt","w", encoding="utf-8") as f:
     f.writelines(cleaned)
+    

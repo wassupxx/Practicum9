b974576a0d806f67a0f680e361c2ee5e621ae280
wassupx <sonechka05062007@gmail.com>
2026-03-14 11:06:43 +0700

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -11,12 +11,14 @@ with open("input.txt", "r", encoding="utf-8") as f:
         with open("output.txt", "w", encoding="utf-8") as fl:
             fl.write("data error")
             exit()
+            
     try:
         result = (num_1 / num_2) + num_3
     except ZeroDivisionError:
         with open("output.txt", "w", encoding="utf-8") as fl:
             fl.write("division by 0")
             exit()
+            
     with open("output.txt", "w", encoding="utf-8") as fe:
         fe.write(str(result))
 
